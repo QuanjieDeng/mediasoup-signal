3def2ba334773a62c3e2eeefc40aead87cf5996e
Quanjie.Deng <www761387265@163.com>
2020-12-18 12:24:40 +0800

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -2,15 +2,15 @@
 
 ## 创建临时文件夹
 WORKERDIR="MEDIASOUP_SIGNALE_DAILYBUILD"
-GIT_USER="dengquanjie"
-GIT_PWD="QuanjieDeng%4011"
+GIT_USER="XXXXX"
+GIT_PWD="XXXXXXX"
 DOCKER_NAME="mediasoup-signal"
 DOCKER_TAG="vv"
 
 rm -rf   ${WORKERDIR}
 mkdir      ${WORKERDIR}
 cd ${WORKERDIR}
-git clone https://${GIT_USER}:${GIT_PWD}@git.devcloud.ztgame.com/realtimevoice/mediasoup-signal.git
+git clone https://${GIT_USER}:${GIT_PWD}@XXXXXXXXXXXX
 
 cd mediasoup-signal
 docker  build  -t  ${DOCKER_NAME}:${DOCKER_TAG}   ./
